02e103a659d0e3533534a8de04409ecae8789382
0minKoh <esesang13@naver.com>
2025-07-10 16:18:18 +0900

    실제 Google Play 리뷰 기반 Slack 자동 보고 통합 스크립트 추가

---
+++ send_google_play_review_report.py
@@ -0,0 +1,15 @@
+from google_play.review_collector import GooglePlayReviewCollector
+from reports.reporter import format_google_play_report
+from reports.slack_reporter import post_slack_thread_reply
+
+# 환경설정: 실제 값으로 교체하거나 환경변수로 관리 가능
+APP_ID = 'com.supaja.hero'
+CHANNEL_ID = 'C0802303F8U'
+THREAD_TS = '1752113397.669759'
+
+if __name__ == '__main__':
+    collector = GooglePlayReviewCollector(app_id=APP_ID)
+    reviews = collector.collect_reviews()
+    message = format_google_play_report(reviews)
+    resp = post_slack_thread_reply(CHANNEL_ID, THREAD_TS, message)
+    print('Slack 응답:', resp)
